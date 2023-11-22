778d6ba19217d6f93e26e75db7ad1fb91b1676e6
keith <keith.wachira@students.moringaschol.com>
2023-11-22 10:35:55 +0300

    order

---
+++ src/components/Order.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Order({ orderData, onDeleteOrder, setOrderData }) {
+  return (
+    <div className="cart-box">
+      <img src={orderData.orderFoodImage} alt="" className="cart-img" />
+      <div className="detail-box">
+        <div className="food-title">{orderData.orderFoodName}</div>
+        <div className="food-price">Ksh {orderData.orderTotalPrice}</div>
+        <span>{orderData.orderQuantity}</span>
+      </div>
+      <button onClick={() => onDeleteOrder(orderData)}>
+        <span>
+          <i className="bx bxs-trash cart-remove m-2"></i>
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default Order;
