c8edf22dc6fd6c718ee46af7c6d84d7f1b85d62e
keith <keith.wachira@students.moringaschol.com>
2023-11-22 10:39:38 +0300

    restaurants

---
+++ src/components/Restaurants.js
@@ -0,0 +1,42 @@
+
+import Restaurant from "./Restaurant";
+
+function Restaurants({ restaurants, setSearchTerm, searchTerm }) {
+  function handleOnSearchRestaurant(e) {
+    setSearchTerm(e.target.value);
+  }
+  return (
+    <>
+      <h2 className="restaurant-browse">Browse Restaurants</h2>
+
+      <form className="d-flex my-4">
+        <input
+          className="form-control me-2 search-box"
+          type="search"
+          placeholder="Search For Your Favorite Restaurant"
+          aria-label="Search"
+          value={searchTerm}
+          onChange={handleOnSearchRestaurant}
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="container-fluid restaurant p-5 shadow">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3">
+          {restaurants
+            .filter((restaurant) => {
+              return searchTerm.toLowerCase() === ""
+                ? restaurant
+                : restaurant.name.toLowerCase().includes(searchTerm);
+            })
+            .map((restaurant, index) => (
+              <Restaurant restaurant={restaurant} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Restaurants;
